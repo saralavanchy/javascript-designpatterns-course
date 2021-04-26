5f27f71b4ff99542937af05f81ac6a6b152a63cb
Sarah <33396785+saralavanchy@users.noreply.github.com>
2021-04-26 10:00:05 -0300

    clicker v2

--- cat_clicker/src/clicker.js
+++ cat_clicker/src/clicker.js
@@ -1,13 +1,15 @@
-
-const handleImageClick = (elementId) => {
-    const clicksElement = document.getElementById(elementId);
-    if(clicksElement){
-        const clicks = clicksElement.innerHTML || 0;
-        clicksElement.innerHTML = parseInt(clicks) + 1;
-    }
+const image = (componentId, src, elementId) => {
+    const img = document.createElement('img');
+    img.id = `img-${componentId}`;
+    img.src = src;
+    img.onclick = function() {handleImageClick(elementId)};
+    img.style.borderRadius="8px";
+    img.style.width="500px";
+    img.style.height="400px";
+    return img;
 }
 
-const textComponent = (componentId, text) => {
+const Text = (componentId, text) => {
     const div = document.createElement('div');
     div.id="text";
     div.innerHTML = `
@@ -16,7 +18,7 @@ const textComponent = (componentId, text) => {
     return div;
 }
 
-const titleComponent = (componentId, text) => {
+const Title = (componentId, text) => {
     const div = document.createElement('div');
     div.id="heading";
     div.innerHTML = `
@@ -24,32 +26,35 @@ const titleComponent = (componentId, text) => {
     `;
     return div;
 }
-const imageComponent = (componentId, src, onclick) => {
-    const div = document.createElement('div');
-    div.id = "img";
-    div.innerHTML = `
-        <img id="${componentId}" src="${src}" onclick="${onclick}" />
-    `;
-    return div;
+
+function handleImageClick(elementId) {
+    const clicksElement = document.getElementById(elementId);
+    console.log()
+    if(clicksElement){
+        const clicks = parseInt(clicksElement.innerHTML) ? parseInt(clicksElement.innerHTML) : 0;
+        clicksElement.innerHTML = parseInt(clicks) + 1;
+    }
 }
 
 const catClicker = (componentId, title, text, imgSrc) => {
     const textId= `clickerText-${componentId}`;
     const div = document.createElement('div');
-    div.id="catClicker";
-    const onImageClick = handleImageClick(textId);
-    div.appendChild(titleComponent(`title-${componentId}`,title));
-    div.appendChild(imageComponent(`image-${componentId}`, imgSrc, onImageClick));
-    div.appendChild(textComponent(textId, text));
+    div.id="catClicker-component";
+    div.appendChild(Title(`title-${componentId}`,title));
+    div.appendChild(image(`image-${componentId}`, imgSrc, textId));
+    div.appendChild(Text(textId, text));
     return div;
 }
 
 const bodyContent = () => {
     const innitialText = "You have not clicked the image yet";
-    const catClicker1 = catClicker("cat-clicker-1", "First Cat", innitialText, "./images/cat-banner.png");
-    const catClicker2 = catClicker("cat-clicker-2", "Second Cat", innitialText, "./images/cat2.jpg");
-    document.getElementById('body-content').appendChild(catClicker1);
-    document.getElementById('body-content').appendChild(catClicker2);
+    const div = document.createElement('div');
+    div.style.display="flex";
+    div.style.flexDirection="row";
+    div.id="catClicker-row";
+    div.appendChild(catClicker("cat-clicker-1", "First Cat", innitialText, "./images/cat-banner.png"));
+    div.appendChild(catClicker("cat-clicker-2", "Second Cat", innitialText, "./images/cat2.jpg"));
+    document.getElementById('body-content').appendChild(div);
 }
 
 window.onload = function() {
